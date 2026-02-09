dee60d6263651764c44ee3679e7b067b7766e8b1
Jonny Burger <jonathanburger11@gmail.com>
2026-02-09 16:04:33 +0100

    Update template

--- src/remotion/MyComp/Main.tsx
+++ src/remotion/MyComp/Main.tsx
@@ -1,39 +1,12 @@
-import { z } from "zod";
-import {
-	AbsoluteFill,
-	Sequence,
-	spring,
-	useCurrentFrame,
-	useVideoConfig,
-} from "remotion";
-import { CompositionProps } from "../../../types/constants";
-import { NextLogo } from "./NextLogo";
-import { loadFont, fontFamily } from "@remotion/google-fonts/Inter";
-import { Rings } from "./Rings";
-import { TextFade } from "./TextFade";
+import { AbsoluteFill } from "remotion";
+import { loadFont } from "@remotion/google-fonts/Inter";
 import { Video } from "@remotion/media";
 
 loadFont("normal", {
 	subsets: ["latin"],
 	weights: ["400", "700"],
 });
-export const Main = ({ title }: z.infer<typeof CompositionProps>) => {
-	const frame = useCurrentFrame();
-	const { fps } = useVideoConfig();
-
-	const transitionStart = 2 * fps;
-	const transitionDuration = 1 * fps;
-
-	const logoOut = spring({
-		fps,
-		frame,
-		config: {
-			damping: 200,
-		},
-		durationInFrames: transitionDuration,
-		delay: transitionStart,
-	});
-
+export const Main = () => {
 	return (
 		<AbsoluteFill className="bg-white">
 			<Video src={"https://remotion.media/video.mp4"} />
